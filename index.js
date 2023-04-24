b4bf2432d83cd19ad532efab0093ca43c3b1fa72
Louis Klein <louisklein71@gmail.com>
2023-04-23 20:00:05 -0700

    test

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names = []
+function writeCards(names, event){
+    var cards = [];
+    for(let i=0; i<names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+function countDown(count){
+    var count
+    while (count >= 0){
+        console.log(count);
+        count--;
+    }
+}
